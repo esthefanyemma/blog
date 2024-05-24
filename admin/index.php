689b1cddcb26a8d260c7df8b164c946cc9487258
Esthefany <esthefanyemmanuelesc@gmail.com>
2024-05-23 23:42:31 -0300

    .

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<?php
+
+require '../config.php';
+include '../src/Artigo.php';
+$artigo = new Artigo($mysql);
+$artigos = $artigo->exibirTodos();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Página administrativa</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Página Administrativa</h1>
+        <div>
+            <div id="artigo-admin">
+                <p>Primeiros passos com Spring</p>
+                <nav>
+                    <a class="botao" href="admin/editar-artigo.html">Editar</a>
+                    <a class="botao" href="admin/excluir-artigo.html">Excluir</a>
+                </nav>
+            </div>
+            <div id="artigo-admin">
+                <p>O que é Metodologia Ágil?</p>
+                <nav>
+                    <a class="botao" href="admin/editar-artigo.html">Editar</a>
+                    <a class="botao" href="admin/excluir-artigo.html">Excluir</a>
+                </nav>
+            </div>
+            <div id="artigo-admin">
+                <p>Como é o funil do Growth Hacking?</p>
+                <nav>
+                    <a class="botao" href="admin/editar-artigo.html">Editar</a>
+                    <a class="botao" href="admin/excluir-artigo.html">Excluir</a>
+                </nav>
+            </div>
+        </div>
+        <a class="botao botao-block" href="admin/adicionar-artigo.html">Adicionar Artigo</a>
+    </div>
+</body>
+
+</html>
